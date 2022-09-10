3759e2e2ee738a54657cd28c6a8c1ab7924aef60
David Becerra <dvd_becerra@outlook.com>
2022-09-10 09:07:55 -0700

    Some bug fixes on gradle, and SQL Connections

--- app/src/main/java/com/example/biomedtech/MainActivity.java
+++ app/src/main/java/com/example/biomedtech/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.biomedtech;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
+
+import com.example.biomedtech.databasemodule.DataBaseHelper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +15,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //DataBaseHelper dbh = DataBaseHelper.getInstance();
+        //dbh.connect();
+
+
     }
 
-
+    @SuppressLint("new api")
+    public void login(View view) {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        DataBaseHelper DBH = DataBaseHelper.getInstance();
+        DBH.connect();
+    }
 }
--- app/src/main/java/com/example/biomedtech/databasemodule/DataBaseHelper.java
+++ app/src/main/java/com/example/biomedtech/databasemodule/DataBaseHelper.java
@@ -1,5 +1,9 @@
 package com.example.biomedtech.databasemodule;
 import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ConcurrentModificationException;
 
 public final class DataBaseHelper {
 
@@ -13,12 +17,44 @@ public final class DataBaseHelper {
         return  instance;
     }
 
-
+    public void connect()
+    {
+        try
+        {
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(ConnectionString());
+            Statement st = conn.createStatement();
+            String sql = "CREATE TABLE Shit (\n" +
+                    "    PersonaID int,\n" +
+                    "    Apellido varchar(255),\n" +
+                    "    Nombre varchar(255),\n" +
+                    "    Direccion varchar(255),\n" +
+                    "    Ciudad varchar(255) \n" +
+                    ");";
+            st.executeQuery(sql);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
 
     //PRIVATE
     private static DataBaseHelper instance;
     private DataBaseHelper()
     {
         // creating data base
+    }
+    private String ConnectionString()
+    {
+        return "jdbc:jtds:sqlserver://biomedtechproyect.database.windows.net:1433;" +
+                "DatabaseName=biomedProyect;" +
+                "user=SA_Biomed@biomedtechproyect;" +
+                "password=418B1omed#;" +
+                "encrypt=false;" +
+                "trustServerCertificate=true;" +
+                "hostNameInCertificate=*.database.windows.net;" +
+                "loginTimeout=30;" +
+                ";ssl=request";
     }
 }
